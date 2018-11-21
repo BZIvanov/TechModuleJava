884ca08c1480ec0267e1955a69071752a1a546a4
BZIvanov <ivanov.biser@mail.bg>
2018-11-21 12:46:34 +0200

    update

---
+++ 05.Lists/cMergingLists.java
@@ -0,0 +1,40 @@
+package company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class cMergingLists {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        List<Integer> first = Arrays.stream(sc.nextLine().split(" "))
+                .map(Integer::parseInt).collect(Collectors.toList());
+
+        List<Integer> second = Arrays.stream(sc.nextLine().split(" "))
+                .map(Integer::parseInt).collect(Collectors.toList());
+
+        List<Integer> result = new ArrayList<>();
+
+        int minSizeList = Math.min(first.size(), second.size());
+        int maxSizeList = Math.max(first.size(), second.size());
+        boolean firstLonger = first.size() > second.size();
+
+        for (int i = 0; i < minSizeList; i++) {
+            result.add(first.get(i));
+            result.add(second.get(i));
+        }
+
+        for (int i = minSizeList; i < maxSizeList; i++) {
+            if (firstLonger) {
+                result.add(first.get(i));
+            } else {
+                result.add(second.get(i));
+            }
+        }
+
+        System.out.println(result.toString().replaceAll("[\\[\\],]", ""));
+    }
+}
