b95f460faf198259a077bdbae80e1a8e3dd53a17
BZIvanov <ivanov.biser@mail.bg>
2019-04-30 17:17:44 +0300

    update

--- 06.ObjectsAndClasses/GAdvertisementMessage.java
+++ 06.ObjectsAndClasses/GAdvertisementMessage.java
@@ -1,5 +1,3 @@
-package com.company;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,5 +7,11 @@ public class GAdvertisementMessage {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(reader.readLine());
+
+        GMessage message = new GMessage();
+        for (int i = 0; i < n; i++) {
+            String msg = message.randomMessage();
+            System.out.println(msg);
+        }
     }
 }
